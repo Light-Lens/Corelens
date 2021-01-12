82807c0f4a99b0f1f87be5e21cf93d6fc29d59f3
Light-Lens <73633374+Light-Lens@users.noreply.github.com>
2021-01-12 13:22:01 +0530

    Fixed the virus detecting engine

--- Program.cs
+++ Program.cs
@@ -8,11 +8,33 @@ namespace Corelens
         static void Main(string[] args)
         {
             Console.Title = "Corelens";
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
             Console.WriteLine("Corelens");
-            Console.Write("Enter your File path with it's name: ");
-            string FilePath = Console.ReadLine();
-            if (File.Exists(FilePath)) File.Delete(FilePath);
-            else Console.WriteLine("Your requested file does not exist.");
+            string[] Colors = {
+            "1 ->       System scan",
+            "2 ->       Custom scan"
+            };
+            for (int i = 0; i < Colors.Length; i++)
+            {
+                Console.WriteLine(Colors[i]);
+            }
+            Console.Write("\nEnter the number> ");
+            ConsoleKeyInfo Control = Console.ReadKey();
+            string GetKey = Control.Key.ToString();
+            Console.WriteLine("");
+            if (GetKey == "D1")
+            {
+                string WinDir = Path.GetPathRoot(Environment.SystemDirectory);
+            }
+
+            else if (GetKey == "D2")
+            {
+                // code here.
+            }
+
+            else Console.WriteLine($"Cannot find any feature for {GetKey}!");
         }
     }
 }
