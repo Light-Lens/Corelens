b78ff7c1c7b1d7cc797e6e576d5fe52282880b1c
Light-Lens <73633374+Light-Lens@users.noreply.github.com>
2021-01-24 19:24:50 +0530

    Add some GUI Elements to our Antivirus

--- Corelet.py
+++ Corelet.py
@@ -6,8 +6,8 @@ import time
 import sys
 import os
 
+from GUI import GUI
 import LOG
-import GUI
 
 # This import blocks pygame from printing it's startup text.
 from os import environ
@@ -22,18 +22,42 @@ pygame.init()
 Loop = True
 Logo = pygame.image.load("Logo.png")
 clock = pygame.time.Clock()
-Display = pygame.display.set_mode((475, 575))
+Display = pygame.display.set_mode((475, 650))
 pygame.display.set_caption("Corelet")
 pygame.display.set_icon(Logo)
 
 os.system('title Corelet')
 LOG.CORELET_LOG(Fore.GREEN + "Corelet")
 
+# Virus names
+Virus_names = "virus.exe", "exploit.application", "internet_explorer.exe", "fake_virus.bat"
+
+# This function will check whether it should allow any program or not.
+def IsVirus(files):
+	if files in Virus_names: return True
+	else: return False
+
+# Main Antivirus Loop
+Title = GUI.Label(Display, (25, 25, 25), 0, 0, 475, 70)
+Title_Text = GUI.Text(Display, "Corelet", (90, 90, 90), (160, 10), 50)
+
+Change = GUI.Button(Display, (50, 50, 50), (70, 70, 70), (40, 40, 40), 10, 100, 115, 30)
+Change_Text = GUI.Text(Display, "System scan", (255, 255, 255), (15, 104), 21)
+Change_Border = GUI.Label(Display, (18, 18, 18), 10, 130, 115, 2)
+Change_Left_Border = GUI.Label(Display, (18, 18, 18), 10, 100, 2, 30)
 while Loop:
-	clock.tick(60)
-	Display.fill((255, 255, 255))
+	clock.tick(120)
+	Display.fill((44, 44, 44))
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: sys.exit()
+
+	Title.draw()
+	Title_Text.draw()
+
+	Change.draw()
+	Change_Text.draw()
+	Change_Border.draw()
+	Change_Left_Border.draw()
 
 	pygame.display.update()
 pygame.quit()
--- GUI.py
+++ GUI.py
@@ -7,18 +7,19 @@ from pygame.locals import *
 # All engine featues
 class GUI:
 	class Label:
-		def __init__(self, Colors, Posx, Posy, Sizex, Sizey):
+		def __init__(self, Graphics, Colors, Posx, Posy, Sizex, Sizey):
 			self.Posx = Posx
 			self.Posy = Posy
 			self.Sizex = Sizex
 			self.Sizey = Sizey
 			self.Colors = Colors
+			self.Graphics = Graphics
 
 		def draw(self):
-			pygame.draw.rect(Display, self.Colors, (self.Posx, self.Posy, self.Sizex, self.Sizey))
+			pygame.draw.rect(self.Graphics, self.Colors, (self.Posx, self.Posy, self.Sizex, self.Sizey))
 
 	class Button:
-		def __init__(self, Colors, NewColor, Pressed, Posx, Posy, Sizex, Sizey):
+		def __init__(self, Graphics, Colors, NewColor, Pressed, Posx, Posy, Sizex, Sizey):
 			self.Posx = Posx
 			self.Posy = Posy
 			self.Sizex = Sizex
@@ -26,29 +27,31 @@ class GUI:
 			self.Colors = Colors
 			self.NewColor = NewColor
 			self.Pressed = Pressed
+			self.Graphics = Graphics
 
 		def draw(self):
-			pygame.draw.rect(Display, self.Colors, (self.Posx, self.Posy, self.Sizex, self.Sizey))
+			pygame.draw.rect(self.Graphics, self.Colors, (self.Posx, self.Posy, self.Sizex, self.Sizey))
 			self.mouseX, self.mouseY = pygame.mouse.get_pos()
 			if self.Posx + self.Sizex > self.mouseX > self.Posx and self.Posy + self.Sizey > self.mouseY > self.Posy:
-				pygame.draw.rect(Display, self.NewColor, (self.Posx, self.Posy, self.Sizex, self.Sizey))
+				pygame.draw.rect(self.Graphics, self.NewColor, (self.Posx, self.Posy, self.Sizex, self.Sizey))
 				if pygame.mouse.get_pressed()[0]:
-					pygame.draw.rect(Display, self.Pressed, (self.Posx, self.Posy, self.Sizex, self.Sizey))
+					pygame.draw.rect(self.Graphics, self.Pressed, (self.Posx, self.Posy, self.Sizex, self.Sizey))
 					pygame.time.delay(100)
 					return 1
 
 				else: return 0
 
-			else: pygame.draw.rect(Display, self.Colors, (self.Posx, self.Posy, self.Sizex, self.Sizey))
+			else: pygame.draw.rect(self.Graphics, self.Colors, (self.Posx, self.Posy, self.Sizex, self.Sizey))
 
 	class Text:
-		def __init__(self, Text, Colors, Font_Pos, Font_size):
+		def __init__(self, Graphics, Text, Colors, Font_Pos, Font_size):
 			self.Colors = Colors
 			self.Font_Pos = Font_Pos
 			self.Font_size = Font_size
 			self.Text = Text
+			self.Graphics = Graphics
 
 		def draw(self):
 			self.font = pygame.font.SysFont("calibri", self.Font_size)
 			self.text = self.font.render(self.Text, True, self.Colors)
-			Display.blit(self.text, self.Font_Pos)
+			self.Graphics.blit(self.text, self.Font_Pos)
